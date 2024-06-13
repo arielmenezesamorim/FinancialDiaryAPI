5bb4042c7612c8b02208ea6748bd54e7371faeaa
Ariel Menezes <arielamorimofc@hotmail.com>
2024-06-13 16:27:53 -0300

    Configurações iniciais do projeto, imprementação dos métodos CRUD da entidade Pais

---
+++ Entities/Models/Filtro/Cadastro/PaisFiltro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models.Filtro.Cadastro
+{
+    public class PaisFiltro
+    {
+        public int tamanhoPagina { get; set; }
+        public int numeroPagina { get; set; } = 0;
+        public string nome { get; set; }
+        public int continente { get; set; } = 5;
+        public string colunaOrdem { get; set; }
+        public string ordem { get; set; }
+    }
+}
---
+++ WebApi/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Interfaces.Cadastro;
+using Domain.Interfaces.Generics;
+using Domain.Interfaces.IServices.Cadastro;
+using Domain.Services.Cadastro;
+using Entities.Notification;
+using Infra.Repositories.Cadastro;
+using Infra.Repositories.Generics;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+using System.Data;
+
+namespace WebApi.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            // Connection
+            services.AddScoped<IDbConnection, NpgsqlConnection>();
+
+            //Notificador
+            services.AddScoped<INotificador, Notificador>();
+
+            services.AddScoped<PaisInterface, PaisRepository>();
+            services.AddScoped<PaisIService, PaisService>();
+
+            return services;
+        }
+    }
+}
