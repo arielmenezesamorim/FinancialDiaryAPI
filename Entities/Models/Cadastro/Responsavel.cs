c005956d50b08ccd5f8b02722c6ef19136722d29
ARIEL <ARIEL@DESKTOP-P7OOJ4R>
2024-07-16 17:21:50 -0300

    Ajustes nas entidades

--- Entities/Models/Cadastro/Responsavel.cs
+++ Entities/Models/Cadastro/Responsavel.cs
@@ -13,20 +13,15 @@ namespace Entities.Models.Cadastro
     {
         [Dapper.Contrib.Extensions.Key]
         public int cadtbresponsavel_pkseq { get; set; }
-
         [Required(ErrorMessage = "Nome é obrigatório")]
         public string cadtbresponsavel_nome { get; set; }
-
         [Required(ErrorMessage = "Tipo de inscrição é obrigatório")]
         public int cadtbresponsavel_tipoinsc { get; set; }
-
         [Required(ErrorMessage = "Inscrição é obrigatório")]
         public long cadtbresponsavel_inscricao { get; set; }
-
         public string cadtbresponsavel_telefone { get; set; }
         public string cadtbresponsavel_celular { get; set; }
         public string cadtbresponsavel_email { get; set; }
-
         public int cadtbresponsavel_cep { get; set; }
         public string cadtbresponsavel_endereco { get; set; }
         public string cadtbresponsavel_nro { get; set; }
@@ -34,11 +29,7 @@ namespace Entities.Models.Cadastro
         public string cadtbresponsavel_bairro { get; set; }
         public int? cadtbresponsavel_fkseqcidade { get; set; }
         public string cadtbresponsavel_nroregprofissional { get; set; }
-
         [Dapper.Contrib.Extensions.Write(false)]
         public Cidade fkcidade { get; set; }
-
-        [Dapper.Contrib.Extensions.Write(false)]
-        public string urlprograma { set; get; }
     }
 }
